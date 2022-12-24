d13f699628076d8f5ca72a32a15cdbd5f3df7ef4
Cho Cho Htet <cho.chohtet@codapayments.com>
2022-12-24 14:37:11 +0700

    integrate firebase

---
+++ app/src/main/java/com/example/in_app_message/data/model/LoggedInUser.kt
@@ -0,0 +1,9 @@
+package com.example.in_app_message.data.model
+
+/**
+ * Data class that captures user information for logged in users retrieved from LoginRepository
+ */
+data class LoggedInUser(
+    val userId: String,
+    val displayName: String
+)
---
+++ app/src/main/java/com/example/in_app_message/ui/login/LoginResult.kt
@@ -0,0 +1,9 @@
+package com.example.in_app_message.ui.login
+
+/**
+ * Authentication result : success (user details) or error message.
+ */
+data class LoginResult(
+    val success: LoggedInUserView? = null,
+    val error: Int? = null
+)
